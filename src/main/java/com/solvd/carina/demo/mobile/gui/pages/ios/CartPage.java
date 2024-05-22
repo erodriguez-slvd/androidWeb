2ff484acc2195f7003c92ee8307f0c38dd91d923
Erika Rodriguez <rodriguez.eri92@gmail.com>
2024-05-22 11:04:05 -0300

    CartPage refactor with corrections

--- src/main/java/com/solvd/carina/demo/mobile/gui/pages/ios/CartPage.java
+++ src/main/java/com/solvd/carina/demo/mobile/gui/pages/ios/CartPage.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.FindBy;
 
 @DeviceType(pageType = DeviceType.Type.IOS_PHONE, parentClass = CartPageBase.class)
-public class CartPage extends CartPageBase{
+public class CartPage extends CartPageBase {
     @FindBy(css = "button[data-test-id='cta-top']")
     private ExtendedWebElement checkoutBtn;
 
@@ -45,10 +45,10 @@ public class CartPage extends CartPageBase{
 
     @Override
     public void selectQuantityOptions() {
-        String option="2";
+        String option = "2";
         quantityBtn.click();
         quantityBtn.sendKeys(Keys.DELETE);
-        quantityBtn.type(option+"\n");
+        quantityBtn.type(option + "\n");
     }
 
     @Override
