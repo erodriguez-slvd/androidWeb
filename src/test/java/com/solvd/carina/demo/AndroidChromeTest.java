29a1f7494aa5dc634def3dbe03acd04ce38c54da
Erika Rodriguez <rodriguez.eri92@gmail.com>
2024-05-23 17:54:51 -0300

    Refactor AndroidChromeTest. Add message to Assert method.

--- src/test/java/com/solvd/carina/demo/AndroidChromeTest.java
+++ src/test/java/com/solvd/carina/demo/AndroidChromeTest.java
@@ -3,116 +3,92 @@ package com.solvd.carina.demo;
 import com.solvd.carina.demo.mobile.gui.pages.common.CartPageBase;
 import com.solvd.carina.demo.mobile.gui.pages.common.HomePageBase;
 import com.solvd.carina.demo.mobile.gui.pages.common.ProductDetailPageBase;
-import com.solvd.carina.demo.mobile.gui.pages.common.WelcomePageBase;
 import com.solvd.carina.demo.utils.MobileContextUtils;
-import com.zebrunner.carina.core.IAbstractTest;
-import com.zebrunner.carina.utils.mobile.IMobileUtils;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
-import java.util.Iterator;
-import java.util.Set;
-
-public class AndroidChromeTest implements IAbstractTest, IMobileUtils {
-    @BeforeSuite
-    public void openEbayWeb() throws InterruptedException {
-        WelcomePageBase welcome = initPage(getDriver(), WelcomePageBase.class);
-        welcome.goToEbayWeb();
-        Thread.sleep(5000);
-    }
+public class AndroidChromeTest extends BaseTest {
     @Test(suiteName = "HomeTest")
     public void searchbarTest() throws InterruptedException {
         HomePageBase home = initPage(getDriver(), HomePageBase.class);
         MobileContextUtils contextHelper = new MobileContextUtils();
         contextHelper.switchMobileContext(MobileContextUtils.View.WEB_CHROME);
-        String search="iPad";
+        String search = "iPad";
         home.searchForAProduct(search);
-        Assert.assertTrue(home.doResultsMatchSearch(search));
+        Assert.assertTrue(home.doResultsMatchSearch(search), "The results do not match the searched product.");
     }
+
     @Test(suiteName = "HomeTest")
-    public void carouselTitlesTest(){
+    public void carouselTitlesTest() {
         HomePageBase home = initPage(getDriver(), HomePageBase.class);
         MobileContextUtils contextHelper = new MobileContextUtils();
         contextHelper.switchMobileContext(MobileContextUtils.View.WEB_CHROME);
         home.switchToWindow();
-        Assert.assertTrue(home.areCarouselTitlesPresent());
+        Assert.assertTrue(home.areCarouselTitlesPresent(), "Carrousel's titles are not present.");
     }
+
     @Test(suiteName = "ProductTest")
-    public void productDetailsTest(){
+    public void productDetailsTest() {
         HomePageBase home = initPage(getDriver(), HomePageBase.class);
         MobileContextUtils contextHelper = new MobileContextUtils();
         contextHelper.switchMobileContext(MobileContextUtils.View.WEB_CHROME);
         home.switchToWindow();
-        ProductDetailPageBase product =home.clickOnACarouselProduct();
+        ProductDetailPageBase product = home.clickOnACarouselProduct();
         product.switchToWindow();
         product.isProductTitlePresent();
         product.isProductPricePresent();
-        Assert.assertTrue(product.isBuyNowBtnPresent());
-    }
-    @Test(suiteName = "ProductTest")
-    public void itemIdTest(){
-        HomePageBase home = initPage(getDriver(), HomePageBase.class);
-        MobileContextUtils contextHelper = new MobileContextUtils();
-        contextHelper.switchMobileContext(MobileContextUtils.View.WEB_CHROME);
-        home.switchToWindow();
-        ProductDetailPageBase product =home.clickOnACarouselProduct();
-        product.switchToWindow();
-        Assert.assertTrue(product.isItemIdPresent());
-    }
-    @Test(suiteName = "CartTest")
-    public void addProductToCartTest(){
-        HomePageBase home = initPage(getDriver(), HomePageBase.class);
-        MobileContextUtils contextHelper = new MobileContextUtils();
-        contextHelper.switchMobileContext(MobileContextUtils.View.WEB_CHROME);
-        home.switchToWindow();
-        ProductDetailPageBase product=home.clickOnACarouselProduct();
-        product.switchToWindow();
-        CartPageBase cart=product.clickOnAddToCartBtn();
-        Assert.assertTrue(cart.isCheckoutBtnPresent());
-    }
-    @Test(suiteName = "CartTest")
-    public void deleteProductFromCartTest(){
-        HomePageBase home = initPage(getDriver(), HomePageBase.class);
-        MobileContextUtils contextHelper = new MobileContextUtils();
-        contextHelper.switchMobileContext(MobileContextUtils.View.WEB_CHROME);
-        home.switchToWindow();
-        ProductDetailPageBase product=home.clickOnACarouselProduct();
-        product.switchToWindow();
-        CartPageBase cart=product.clickOnAddToCartBtn();
-        cart.clickOnDeleteBtn();
-        Assert.assertTrue(cart.isConfirmationMessagePresent());
-    }
-    @Test(suiteName = "CartTest")
-    public void changeQuantityTest(){
-        HomePageBase home = initPage(getDriver(), HomePageBase.class);
-        MobileContextUtils contextHelper = new MobileContextUtils();
-        contextHelper.switchMobileContext(MobileContextUtils.View.WEB_CHROME);
-        home.switchToWindow();
-        ProductDetailPageBase product=home.clickOnACarouselProduct();
-        product.switchToWindow();
-        CartPageBase cart=product.clickOnAddToCartBtn();
-        String oldPrice= cart.getProductPrice();
-        cart.selectQuantityOptions();
-        pause(5L);
-        String newPrice=cart.getProductPrice();
-        Assert.assertFalse(oldPrice.equals(newPrice));
+        Assert.assertTrue(product.isBuyNowBtnPresent(), "BuyNow button is not present on Product Details");
     }
 
-    @AfterTest
-    public void closeWindow() {
+    @Test(suiteName = "ProductTest")
+    public void itemIdTest() {
+        HomePageBase home = initPage(getDriver(), HomePageBase.class);
         MobileContextUtils contextHelper = new MobileContextUtils();
         contextHelper.switchMobileContext(MobileContextUtils.View.WEB_CHROME);
-        Set<String> handles = getDriver().getWindowHandles();
-        Iterator it = handles.iterator();
-        String parent = (String) it.next();
-        if (((String) it.next()).isEmpty()) {
-            getDriver().switchTo().window(parent);
-        } else {
-            String child = (String) it.next();
-            getDriver().switchTo().window(child);
-            getDriver().switchTo().window(parent);
-        }
+        home.switchToWindow();
+        ProductDetailPageBase product = home.clickOnACarouselProduct();
+        product.switchToWindow();
+        Assert.assertTrue(product.isItemIdPresent(), "ItemID is not present on Product Details");
+    }
+
+    @Test(suiteName = "CartTest")
+    public void addProductToCartTest() {
+        HomePageBase home = initPage(getDriver(), HomePageBase.class);
+        MobileContextUtils contextHelper = new MobileContextUtils();
+        contextHelper.switchMobileContext(MobileContextUtils.View.WEB_CHROME);
+        home.switchToWindow();
+        ProductDetailPageBase product = home.clickOnACarouselProduct();
+        product.switchToWindow();
+        CartPageBase cart = product.clickOnAddToCartBtn();
+        Assert.assertTrue(cart.isCheckoutBtnPresent(), "Checkout button is not present on Cart Page");
+    }
+
+    @Test(suiteName = "CartTest")
+    public void deleteProductFromCartTest() {
+        HomePageBase home = initPage(getDriver(), HomePageBase.class);
+        MobileContextUtils contextHelper = new MobileContextUtils();
+        contextHelper.switchMobileContext(MobileContextUtils.View.WEB_CHROME);
+        home.switchToWindow();
+        ProductDetailPageBase product = home.clickOnACarouselProduct();
+        product.switchToWindow();
+        CartPageBase cart = product.clickOnAddToCartBtn();
+        cart.clickOnDeleteBtn();
+        Assert.assertTrue(cart.isConfirmationMessagePresent(), "Deletion confirmation message is not shown.");
+    }
+
+    @Test(suiteName = "CartTest")
+    public void changeQuantityTest() {
+        HomePageBase home = initPage(getDriver(), HomePageBase.class);
+        MobileContextUtils contextHelper = new MobileContextUtils();
+        contextHelper.switchMobileContext(MobileContextUtils.View.WEB_CHROME);
+        home.switchToWindow();
+        ProductDetailPageBase product = home.clickOnACarouselProduct();
+        product.switchToWindow();
+        CartPageBase cart = product.clickOnAddToCartBtn();
+        String oldPrice = cart.getProductPrice();
+        cart.selectQuantityOptions();
+        pause(5L);
+        String newPrice = cart.getProductPrice();
+        Assert.assertNotEquals(newPrice, oldPrice, "Total price has not changed.");
     }
 }
